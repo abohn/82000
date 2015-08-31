a53358297c49b6f042972401f94b3dee8ab91cd0
Andy Bohn <adb228@cornell.edu>
2015-08-31 02:44:40 -0400

    Parallelize with OpenMP if available

--- Executables/FindNextNumber.cpp
+++ Executables/FindNextNumber.cpp
@@ -1,7 +1,16 @@
 #include <cmath>
 #include <iostream>
+#include <omp.h>
 
 #include "Tools/BigInt.hpp"
+
+/// Tests if the passed number has only 0s and 1s in the specified base
+/// representation.
+/// The number will be modified to be equal to the next largest number which
+/// satisfies the test in this base
+bool TestNumber(BigInt& num, const size_t kBase);
+/// Uses correct time function when OpenMP is being used
+double GetTimeInSeconds();
 
 // Find the smallest number whose representation in all bases below a specified
 // base only uses 0s and 1s
@@ -12,11 +21,109 @@
 // base <= 3: 3. 11b2, 10b3
 // base <= 4: 4. 100b2, 11b3, 10b4
 // base <= 5: 82000. Note that 5 doesn't work, since 5b10 = 12b3
+int main() {
+  // Some configurables
+  const size_t kMinBase = 3;
+  const size_t kMaxBase = 6;
+  const uint64_t kStartNumLog10 = 1;
 
-/// Tests if the passed number has only 0s and 1s in the specified base
-/// representation.
-/// The number will be modified to be equal to the next largest number which
-/// satisfies the test in this base
+  static_assert(kStartNumLog10 >= 1, "Must start search at 10^1 or above");
+  static_assert(kMinBase <= kMaxBase, "Min base must be <= max base");
+  static_assert((kMaxBase == 5 && kStartNumLog10 <= 4) || kMaxBase >= 6,
+                "Start of search is larger than solution for max base");
+
+  std::cout << "Finding smallest number represented by only 0s and 1s from\n"
+    << "base " << kMinBase << " to " << kMaxBase << ", larger than "
+    << "10^" << kStartNumLog10 << "\n";
+
+  const double kStart = GetTimeInSeconds();
+
+  BigInt answer;
+  BigInt guess(10);
+  guess.RaiseToPower(kStartNumLog10);
+
+  bool foundAnswer = false;
+  // Each thread will check a range of numbers from currentNumBits to
+  // currentNumBits + kNumBitsToCheck, before grabbing the next available
+  // range of numbers
+  uint64_t currentNumBits = guess.SizeInBase(2);
+  // check this many bits, which has log(2)/log(10) times as many digits in base 10
+  // We may want to explore making this proportional to the size of the current
+  // guess, since the guesses should jump proportionally
+  const uint64_t kNumBitsToCheck = 1e3;
+
+  uint numsCheckedThisRange = 0;
+
+#ifdef _OPENMP
+#pragma omp parallel default(none) private(guess, numsCheckedThisRange) \
+    shared(std::cout, currentNumBits, foundAnswer, answer)
+#endif
+  {
+    uint64_t upperBoundNumBitsToCheck;
+
+    while (!foundAnswer) {
+#ifdef _OPENMP
+#pragma omp critical(getBitRange)
+#endif
+      {
+        guess.Set(2, currentNumBits);
+        currentNumBits += kNumBitsToCheck;
+        upperBoundNumBitsToCheck = currentNumBits;
+      }
+
+      //Print out progress
+#ifdef _OPENMP
+#pragma omp master
+#endif
+      {
+        std::cout << guess.SizeInBase(10) << " " << guess.SizeInBase(2) << " "
+                  << (GetTimeInSeconds() - kStart) << " "
+                  << numsCheckedThisRange << "\n";
+      }
+      numsCheckedThisRange = 0;
+
+      while (!foundAnswer && guess.SizeInBase(2) < upperBoundNumBitsToCheck) {
+        bool guessIsCorrect = true;
+        numsCheckedThisRange++;
+
+        // Bail out of the loop as soon as one base fails the test.
+        // Start with the maximum base, so statistically our next guess will be
+        // the farthest away
+        for (size_t iBase = kMaxBase; iBase >= kMinBase && guessIsCorrect;
+             iBase--) {
+          guessIsCorrect = TestNumber(guess, iBase);
+        }
+
+        if (guessIsCorrect) {
+#ifdef _OPENMP
+#pragma omp critical(updateAnswer)
+#endif
+          {
+            if (foundAnswer) {
+              // We've already found an answer, so we need to choose the smaller
+              // of the two
+              std::cout
+                  << "Found another answer.  Saving the smaller of the two\n";
+              if (guess < answer) {
+                answer = guess;
+              }
+            }
+            else {
+              std::cout << "Found an answer\n";
+              foundAnswer = true;
+              answer = guess;
+            }
+          }
+        }
+      }
+    }
+  } // end omp parallel section
+
+  std::cout << "Answer written to answer.txt\n";
+  answer.PrintToFile("answer.txt");
+  return 0;
+}
+
 bool TestNumber(BigInt& num, const size_t kBase) {
   // Test num starting from the largest digit in kBase
   // The check variable stores kBase^n, or 1 followed by 0s in base kBase, and
@@ -75,41 +182,14 @@ bool TestNumber(BigInt& num, const size_t kBase) {
   return valid;
 }
 
-int main() {
-  // Some configurables
-  const size_t kMinBase = 3;
-  const size_t kMaxBase = 5;
-  const size_t kStartNumLog10 = 1;
-
-  static_assert(kStartNumLog10 >= 1, "Must start search at 10^1 or above");
-  static_assert(kMinBase <= kMaxBase, "Min base must be <= max base");
-  static_assert((kMaxBase == 5 && kStartNumLog10 <= 4) || kMaxBase >= 6,
-                "Start of search is larger than solution for max base");
-
-  std::cout << "Finding smallest number represented by only 0s and 1s from\n"
-    << "base " << kMinBase << " to " << kMaxBase << ", larger than "
-    << "10^" << kStartNumLog10 << "\n";
-
-  BigInt answer();
-  bool foundAnswer = false;
-  BigInt guess(10);
-  guess.RaiseToPower(kStartNumLog10);
-
-  while (!foundAnswer) {
-    bool guessIsCorrect = true;
-    guess.PrintToFile("guess.txt");
-
-    // Bail out of the loop as soon as one base fails the test.
-    // Start with the maximum base, so statistically our next guess will be
-    // the farthest away
-    for (size_t iBase = kMaxBase; iBase >= kMinBase && guessIsCorrect;
-         iBase--) {
-      guessIsCorrect = TestNumber(guess, iBase);
-    }
-    foundAnswer = guessIsCorrect;
-  }
-
-  std::cout << "Answer written to answer.txt\n";
-  guess.PrintToFile("answer.txt");
-  return 0;
+double GetTimeInSeconds() {
+  double time;
+#ifdef _OPENMP
+  time = omp_get_wtime();
+#else
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  time = tv.tv_sec + tv.tv_usec / (1.e6);
+#endif
+  return time;
 }
